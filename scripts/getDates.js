c34c1b2913c6e87e6cf80e763c2d9978d582259d
charlestawanda <tsamatanda@gmail.com>
2025-03-17 17:05:04 +0200

    we2

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,18 @@
 const date = document.querySelector("#currentYear")
 const lm = document.querySelector("#lastModified")
+const nav = document.querySelector("nav");
+const courseNav = document.querySelector("#nav");
 
 const dt = new Date();
 date.innerHTML = dt.getFullYear();
 
 lm.innerHTML = `Last Modified: ${document.lastModified}`
+// responsive ham and nav
+ham.addEventListener("click", () => {
+    ham.classList.toggle("show");
+    nav.classList.toggle("show");
+  });
+  // filters and stuff
+courseNav.addEventListener("click", (event) => {
+    getCourses(event.target.textContent);
+  });
